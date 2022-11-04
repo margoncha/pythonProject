fb815ee6441c9942183d4efb3b5d8cc106c187ba
Maria Goncharova <margoncha@bk.ru>
2022-11-04 12:10:28 +0300

    Добавлена переменная text и функция minus

--- main.py
+++ main.py
@@ -3,12 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+text="new text"
 
 def plus(a,b):
     return a+b
 
+def minus(a,b):
+    return a-b
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(plus(2,4))
+    print(minus(4, 2))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
